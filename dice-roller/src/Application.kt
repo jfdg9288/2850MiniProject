76addb1016ee15d80906d1e44579c9b4b9e6f6a3
Samuel J. Roper <149962538+SamuelRoper@users.noreply.github.com>
2026-02-17 14:52:01 +0000

    Add files via upload

---
+++ dice-roller/src/Application.kt
@@ -0,0 +1,12 @@
+// Application entry point
+
+import io.ktor.server.application.Application
+
+fun Application.module() {
+    configureErrorHandling()
+    configureRouting()
+}
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
